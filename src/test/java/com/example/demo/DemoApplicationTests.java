2c7cad552e12e064855f03b6c2adb38f4ccff136
zhengxiaobin <zhengxiaobin@babytree-inc.com>
2019-03-29 15:46:30 +0800

    test properties

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.user.properties.DemoProperties;
+import com.example.demo.user.properties.MyProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+    @Autowired
+    MyProperties myProperties;
+
+    @Autowired
+    DemoProperties demoProperties;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void testProperties(){
+        System.out.println(myProperties.title);
+        System.out.println(myProperties.description);
+    }
+
+    @Test
+    public void testDemoProperties(){
+        System.out.println(demoProperties.title);
+        System.out.println(demoProperties.description);
     }
 
 }
